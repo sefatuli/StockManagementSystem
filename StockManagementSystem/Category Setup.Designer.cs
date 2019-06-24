d341c8388d4c5c73817e505d60692938faf526dc
Aponashik <aponashikmahmud007@gmail.com>
2019-06-25 01:32:00 +0600

    Category Setup

---
+++ StockManagementSystem/Category Setup.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace StockManagementSystem
+{
+    partial class Category_Setup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.catnameTextBox = new System.Windows.Forms.TextBox();
+            this.displaycatDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.displaycatDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(379, 125);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 0;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(151, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // catnameTextBox
+            // 
+            this.catnameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.catnameTextBox.Location = new System.Drawing.Point(226, 81);
+            this.catnameTextBox.Name = "catnameTextBox";
+            this.catnameTextBox.Size = new System.Drawing.Size(228, 22);
+            this.catnameTextBox.TabIndex = 2;
+            // 
+            // displaycatDataGridView
+            // 
+            this.displaycatDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.displaycatDataGridView.Location = new System.Drawing.Point(154, 194);
+            this.displaycatDataGridView.Name = "displaycatDataGridView";
+            this.displaycatDataGridView.Size = new System.Drawing.Size(386, 170);
+            this.displaycatDataGridView.TabIndex = 3;
+            // 
+            // Category_Setup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 439);
+            this.Controls.Add(this.displaycatDataGridView);
+            this.Controls.Add(this.catnameTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SaveButton);
+            this.Name = "Category_Setup";
+            this.Text = "Category_Setup";
+            ((System.ComponentModel.ISupportInitialize)(this.displaycatDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox catnameTextBox;
+        private System.Windows.Forms.DataGridView displaycatDataGridView;
+    }
+}
--- StockManagementSystem/Program.cs
+++ StockManagementSystem/Program.cs
@@ -16,7 +16,8 @@ namespace StockManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+            Application.Run(new Category_Setup());
         }
     }
 }
