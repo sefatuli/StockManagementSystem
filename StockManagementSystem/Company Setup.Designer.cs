0c5da017efcc0b63aab894ee7ba51ed179e48420
Aponashik <aponashikmahmud007@gmail.com>
2019-06-26 18:46:34 +0600

    Company setup

---
+++ StockManagementSystem/Company Setup.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace StockManagementSystem
+{
+    partial class Company_Setup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.companynameTextBox = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(331, 135);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 0;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Company Name";
+            // 
+            // companynameTextBox
+            // 
+            this.companynameTextBox.Location = new System.Drawing.Point(199, 83);
+            this.companynameTextBox.Name = "companynameTextBox";
+            this.companynameTextBox.Size = new System.Drawing.Size(207, 20);
+            this.companynameTextBox.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(90, 213);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(500, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Company_Setup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.companynameTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SaveButton);
+            this.Name = "Company_Setup";
+            this.Text = "Company_Setup";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox companynameTextBox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
--- StockManagementSystem/Program.cs
+++ StockManagementSystem/Program.cs
@@ -17,7 +17,7 @@ namespace StockManagementSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
-            Application.Run(new Category_Setup());
+            Application.Run(new Menu());
         }
     }
 }
