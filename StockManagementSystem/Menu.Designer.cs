9be283efb8753abc20818cf9217e01e950e7df43
sefatuli <sefatuli@gmail.com>
2019-07-06 23:18:36 +0600

    Home page Design

--- StockManagementSystem/Menu.Designer.cs
+++ StockManagementSystem/Menu.Designer.cs
@@ -35,13 +35,15 @@
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(67, 84);
+            this.button1.Location = new System.Drawing.Point(199, 35);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(134, 23);
+            this.button1.Size = new System.Drawing.Size(134, 32);
             this.button1.TabIndex = 0;
             this.button1.Text = "Category Setup";
             this.button1.UseVisualStyleBackColor = true;
@@ -49,9 +51,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(67, 113);
+            this.button2.Location = new System.Drawing.Point(199, 73);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(134, 23);
+            this.button2.Size = new System.Drawing.Size(134, 35);
             this.button2.TabIndex = 1;
             this.button2.Text = "Company Setup";
             this.button2.UseVisualStyleBackColor = true;
@@ -59,9 +61,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(67, 142);
+            this.button3.Location = new System.Drawing.Point(199, 114);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(134, 23);
+            this.button3.Size = new System.Drawing.Size(134, 32);
             this.button3.TabIndex = 2;
             this.button3.Text = "Item Setup";
             this.button3.UseVisualStyleBackColor = true;
@@ -69,9 +71,9 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(67, 171);
+            this.button4.Location = new System.Drawing.Point(199, 154);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(134, 23);
+            this.button4.Size = new System.Drawing.Size(134, 30);
             this.button4.TabIndex = 3;
             this.button4.Text = "Stock In";
             this.button4.UseVisualStyleBackColor = true;
@@ -79,9 +81,9 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(67, 200);
+            this.button5.Location = new System.Drawing.Point(199, 193);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(134, 23);
+            this.button5.Size = new System.Drawing.Size(134, 35);
             this.button5.TabIndex = 4;
             this.button5.Text = "Stock Out";
             this.button5.UseVisualStyleBackColor = true;
@@ -89,9 +91,9 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(67, 230);
+            this.button6.Location = new System.Drawing.Point(199, 234);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(134, 23);
+            this.button6.Size = new System.Drawing.Size(134, 32);
             this.button6.TabIndex = 5;
             this.button6.Text = "Summary";
             this.button6.UseVisualStyleBackColor = true;
@@ -99,28 +101,42 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(67, 260);
+            this.button7.Location = new System.Drawing.Point(199, 272);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(134, 23);
+            this.button7.Size = new System.Drawing.Size(134, 31);
             this.button7.TabIndex = 6;
             this.button7.Text = "Date Wise Summary";
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.groupBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.groupBox1.Location = new System.Drawing.Point(137, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(516, 328);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Stock Management System";
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 352);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Menu";
             this.Text = "Menu";
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -134,5 +150,6 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
